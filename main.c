0699978bfd5b7d51ecca0b9adc9de188d1057403
Samet Comert <cokciddisin@gmail.com>
2020-02-23 01:00:19 +0300

    Split the string

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("ben nie böleyim :/\n");
-    return 1;
+int dictionary();
+
+int main(int argc, char** argv){
+    char input[] = "The cat ate the fish. The dog barked and bit the cat. The dog ate John's bone.";
+    dictionary(input);
+}
+
+int dictionary(char text[]) {
+    char* token = strtok(text, ".");
+    char words[] = "";
+    int sentenceCount = 0;
+
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, ".");
+        sentenceCount++;
+    }
 }
